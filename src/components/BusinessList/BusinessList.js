ee1b673fa29b80fa911f2f132933033817927082
Steve Toro <steve@stevetoro.com>
2018-06-27 22:54:56 -0400

    BusinessList component rendering multiple Business components.

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Business from '../Business/Business';
+import './BusinessList.css';
+
+const BusinessList = () => (
+  <div className="BusinessList">
+    <Business />
+    <Business />
+    <Business />
+    <Business />
+    <Business />
+    <Business />
+  </div>
+);
+
+export default BusinessList;
